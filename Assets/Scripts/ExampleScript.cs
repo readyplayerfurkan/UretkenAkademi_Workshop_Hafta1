64770b3cbb7d27d451acbddda8834bc72a625410
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2024-01-02 19:14:18 +0300

    feat: Created first script to say hello to world :)

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    private void SayHelloToWorld()
+    {
+        Debug.Log("Hello world :/");
+    }
+
+    private void Start()
+    {
+        SayHelloToWorld();
+    }
+    
+}
